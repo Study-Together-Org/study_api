9eced6209ed6f351c3ef735919c187ccdd96cfd1
Cole Killian <g.cole.killian@gmail.com>
2021-07-19 19:36:44 -0700

    update devForward

--- devForward.sh
+++ devForward.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # sql/mariadb ports
-ssh -fN -L 3306:localhost:3306 root@95.179.130.39
+ssh -fN -L 3306:localhost:3306 root@136.244.109.248
 
 # redis ports
-ssh -fN -L 6379:localhost:6379 root@95.179.130.39
+ssh -fN -L 6379:localhost:6379 root@136.244.109.248
 
 # discord ext ipc ports
-ssh -fN -L 20000:localhost:20000 root@95.179.130.39
-ssh -fN -L 8765:localhost:8765 root@95.179.130.39
+# ssh -fN -L 20000:localhost:20000 root@95.179.130.39
+# ssh -fN -L 8765:localhost:8765 root@95.179.130.39
